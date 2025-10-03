38c9d60df17c0ba8e578517a4ac47a2d2d82a40b
Kaio Muniz <kkaioribeiro@gmail.com>
2025-10-03 19:57:53 -0300

    conclui o CategoriaController com o end point de categorias

--- src/main/java/br/com/kaiomuniz/controllers/CategoriaController.java
+++ src/main/java/br/com/kaiomuniz/controllers/CategoriaController.java
@@ -1,13 +1,29 @@
 package br.com.kaiomuniz.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.kaiomuniz.repositories.CategoriaRepository;
 
 @RestController
 @RequestMapping("/api/v1/categorias")
 public class CategoriaController {
-	
-	public void get() {
-		
+
+	@GetMapping
+	public ResponseEntity<?> get() {
+		try {
+
+			var categoriaRepository = new CategoriaRepository();
+			var lista = categoriaRepository.findAll();
+
+			return ResponseEntity.ok()
+					.body(lista);
+		} catch (Exception e) {
+			return ResponseEntity.internalServerError()
+
+					.body(e.getMessage());
+		}
 	}
 }
