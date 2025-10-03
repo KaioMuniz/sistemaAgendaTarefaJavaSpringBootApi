4692fae6597f43934f9df19168e9ddd2aafece95
Kaio Muniz <kkaioribeiro@gmail.com>
2025-10-03 18:24:03 -0300

    projeto base iniciado apenas com spring web e spring dev tools

---
+++ src/main/java/br/com/kaiomuniz/SistemaAgendaTarefaJavaSpringBootApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.kaiomuniz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAgendaTarefaJavaSpringBootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAgendaTarefaJavaSpringBootApiApplication.class, args);
+	}
+
+}
