d46997733cf094451ebfb2bf436999920bfea62e
Kaio Muniz <kkaioribeiro@gmail.com>
2025-10-03 19:54:26 -0300

    completei a classe CategoriaRepository com o metodo de buscar todas as categorias

--- src/main/java/br/com/kaiomuniz/repositories/CategoriaRepository.java
+++ src/main/java/br/com/kaiomuniz/repositories/CategoriaRepository.java
@@ -1,13 +1,40 @@
 package br.com.kaiomuniz.repositories;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import br.com.kaiomuniz.entities.Categoria;
+import br.com.kaiomuniz.factories.ConnectionFactory;
 
 public class CategoriaRepository {
 
-
+	// Método para consultar todas as categorias
 	public List<Categoria> findAll() throws Exception {
-		return null;
+
+		var sql = """
+					select id, nome from categoria
+					order by nome asc
+				""";
+
+		var connection = ConnectionFactory.getConnection();
+
+		var statement = connection.prepareStatement(sql);
+		var result = statement.executeQuery();
+
+		var lista = new ArrayList<Categoria>();
+
+		while (result.next()) {
+
+			var categoria = new Categoria();
+
+			categoria.setId(UUID.fromString(result.getString("id")));
+			categoria.setNome(result.getString("nome"));
+
+			lista.add(categoria);
+		}
+
+		connection.close();
+		return lista;
 	}
 }
